41feb5311819ba8a4a39302b065667c54cac940e
anon454 <antwoine31@gmail.com>
2020-10-05 11:59:39 +0200

    Initial commit

--- process_pc.sh
+++ process_pc.sh
@@ -1,5 +1,5 @@
 ply_path=$1
-ransac_code_path=$2
+tree_code_path=$2
 
 surface_dir="dataset/"surface
 color_dir="dataset/"color
@@ -9,15 +9,15 @@ rm -rf $color_dir
 mkdir -p $surface_dir
 mkdir -p $color_dir
 
-build_path=$ransac_code_path/"build"
+build_path=$tree_code_path/"build"
 
 for pc in $(ls ${ply_path})
 do
   echo "Processing file: "$ply_path/$pc
-  ./$build_path/ransac_tree $ply_path/$pc
+  ./$build_path/pc_tree $ply_path/$pc
   file_name="${pc%%.*}"
-  mv $ransac_code_path/"texture0.png" $color_dir/$file_name".png"
-  mv $ransac_code_path/"rad.txt" $surface_dir/$file_name".txt"
+  mv $tree_code_path/"texture0.png" $color_dir/$file_name".png"
+  mv $tree_code_path/"rad.txt" $surface_dir/$file_name".txt"
 done
 python convert_to_surface.py --surface_path $surface_dir
 echo "Extracted the surface and color"
